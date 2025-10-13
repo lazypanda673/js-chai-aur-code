b2952ef3ab5a35f3ead4b5743d2eab79cc6311cd
Sumit Kumar <sumitsonata673@gmail.com>
2025-10-13 16:39:51 +0530

    dataTypes in JS

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // strictly uses newer version of JS
+
+// alert(3 + 3); // works in browser, we are using node.js
+
+// always keep the code readability high
+console.log(3 + 3)
+console.log("Sumit")
+
+//Data Types
+let name = "sumit"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value // why null is an object of type
+// undefined => no value assigned // while undefined is "undefined" of type
+// symbol => for uniqueness
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
